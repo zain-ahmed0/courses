f8006014a00d23b393333ce1d92ee2e3bb9823ee
zain-ahmed <zain.auk05@gmail.com>
2024-12-20 16:56:01 +0000

    Rename course folders

---
+++ Foundational C# with Microsoft/Course 3/Module 1/Exercise 2/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+string value1 = " a";
+string value2 = " A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+*/
+
+/*
+Console.WriteLine("a" != "a");
+Console.WriteLine("b" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
+*/
+
+/*
+Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
+*/
+
+/*
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+*/
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
