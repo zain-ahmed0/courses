7ec521d864c3cbf6d8530cae85c5887b9e8c2684
zain-ahmed <zain.auk05@gmail.com>
2024-12-20 18:12:42 +0000

    Complete Module 2

---
+++ Foundational C# with Microsoft/Course 3/Module 2/Exercise 2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+// Code sample 1
+bool flag = true;
+int value = 0;
+
+if (flag)
+{
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+value = 10;
+Console.WriteLine($"Inside the code block: {value}");
+*/
+
+// Code sample 2
+int value;
+
+if (true)
+{
+    value = 10;
+    Console.WriteLine($"Inside the code block: {value}");
+}
+
+Console.WriteLine($"Outside the code block: {value}");
---
+++ Foundational C# with Microsoft/Course 3/Module 2/Exercise 3/Program.cs
@@ -0,0 +1,9 @@
+﻿string name = "steve";
+if (name == "bob") 
+    Console.WriteLine("Found Bob");
+else if (name == "steve") 
+    Console.WriteLine("Found Steve");
+else 
+    Console.WriteLine("Found Chuck");
+
+
