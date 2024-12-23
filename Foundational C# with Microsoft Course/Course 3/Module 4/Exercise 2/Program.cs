4b18ad46bb8d201916d0b24db8fdee811d71595d
zain-ahmed <zain.auk05@gmail.com>
2024-12-23 13:10:58 +0000

    Complete Module 4

---
+++ Foundational C# with Microsoft Course/Course 3/Module 4/Exercise 2/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] names = ["Alex", "Eddie", "David", "Michael"];
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+
+
