b2cfeff5e647c002602f417acedf382b3d2404ed
zain-ahmed <zain.auk05@gmail.com>
2024-12-23 18:23:33 +0000

    Complete Module 5

---
+++ Foundational C# with Microsoft Course/Course 3/Module 5/Exercise 3/Program.cs
@@ -0,0 +1,22 @@
+﻿int heroHealth = 10;
+int monsterHealth = 10;
+Random attack = new Random();
+
+while (monsterHealth > 0 && heroHealth > 0)
+{
+    
+    int damage = attack.Next(1, 11);
+    Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth - damage} health.");
+    monsterHealth -= damage;
+    
+    if (monsterHealth > 0)
+    {
+        damage = attack.Next(1, 11);
+        Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth - damage} health.");
+        heroHealth -= damage;
+    }
+}
+
+string winner = heroHealth > monsterHealth ? "Hero wins!" : "Monster wins!";
+Console.WriteLine(winner);
+
---
+++ Foundational C# with Microsoft Course/Course 3/Module 5/Exercise 5/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+string? readResult;
+bool validNumber;
+
+Console.WriteLine("Enter a number between 5 and 10: ");
+
+do
+{
+    readResult = Console.ReadLine();
+    int number = 0;
+    validNumber = int.TryParse(readResult, out number);
+
+    if (validNumber == false)
+    {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
+    }
+    
+    if (number >= 5 && number <= 10)
+    {
+        Console.WriteLine($"Your input value ({number}) has been accepted.");
+    }
+    else
+    {
+        Console.WriteLine($"You entered {number}. Please enter a number between 5 and 10.");
+    }
+
+} while (!validNumber);
+*/
+
+/*
+string? readResult;
+bool validEntry = false;
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do
+{
+    readResult = Console.ReadLine();
+    string newString = readResult.Trim().ToLower();
+
+    if (newString == "administrator" || newString == "manager" || newString == "user")
+    {
+        validEntry = true;
+        Console.WriteLine("Accepted");
+    }
+    else
+    {
+        validEntry = false;
+        Console.WriteLine("Incorrect input");
+    }
+
+} while (validEntry == false);
+*/
+
+string[] myStrings = ["I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"];
+int stringsCount = myStrings.Length;
+
+string myString = "";
+int periodLocation = 0;
+
+for (int i = 0; i < stringsCount; i++)
+{
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+
+    string mySentence;
+    
+    while (periodLocation != -1)
+    {
+        mySentence = myString.Remove(periodLocation);
+
+        myString = myString.Substring(periodLocation + 1);
+
+        myString = myString.TrimStart();
+
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+    
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
