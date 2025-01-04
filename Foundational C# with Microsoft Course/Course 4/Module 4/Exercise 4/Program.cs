0a7ce47556779bd54bc29607a2a186c0ed1886cc
zain-ahmed <zain.auk05@gmail.com>
2025-01-04 22:00:17 +0000

    Complete Module 4

---
+++ Foundational C# with Microsoft Course/Course 4/Module 4/Exercise 4/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"   Shares: {productShares:N3} Product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+*/
+
+/*
+string input = "Pad this";
+
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12));
+
+Console.WriteLine("");
+
+Console.WriteLine(input.PadLeft(12, '-'));
+Console.WriteLine(input.PadRight(12, '-'));
+*/
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
---
+++ Foundational C# with Microsoft Course/Course 4/Module 4/Exericse 2/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+string first = "Hello";
+string second = "World";
+Console.WriteLine("{1} {0}!", first, second);
+Console.WriteLine("{0} {0} {0}!", first, second);
+*/
+
+/*
+string first = "Hello";
+string second = "World";
+Console.WriteLine($"{first} {second}!");
+Console.WriteLine($"{second} {first}!");
+Console.WriteLine($"{first} {first} {first}!");
+*/
+
+/*
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+*/
+
+/*
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+*/
+
+/*
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+*/
+
+decimal price = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("You saved {0:C2} off the regular {1:C2} price. ", (price - salePrice), price);
+
+yourDiscount += $"A discount of {((price - salePrice)/price):P2}!"; //inserted
+Console.WriteLine(yourDiscount);
+
+
+
+
+
