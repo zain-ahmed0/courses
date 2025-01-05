f8f46d41e4654a5bda7d5865edfc26109280bbc3
zain-ahmed <zain.auk05@gmail.com>
2025-01-05 23:58:38 +0000

    Complete Exercise 2, 3 and 4

---
+++ Foundational C# with Microsoft Course/Course 4/Module 5/Exercise 2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+string message = "Find what is (inside the parentheses)";
+   
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+   
+openingPosition += 1;
+   
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
