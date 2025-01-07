9826e69dc74a0e3270752652654a60a9e00c0648
zain-ahmed <zain.auk05@gmail.com>
2025-01-07 17:17:25 +0000

    Complete Module 1 - Exercise 2

---
+++ Foundational C# with Microsoft Course/Course 5/Module 1/Exercise 2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+SayHello();
+
+void SayHello()
+{
+    Console.WriteLine("Hello World!");
+}
+*/
+
+/*
+int[] a = {1,2,3,4,5};
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+
+void PrintArray()
+{
+    foreach (int x in a)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
+*/
+
+Console.WriteLine("Before calling a method");
+SayHello();
+Console.WriteLine("After calling a method");
+
+void SayHello() 
+{
+    Console.WriteLine("Hello World!");
+}
