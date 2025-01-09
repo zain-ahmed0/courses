1df099e42d322c60e589e1a7551a893b61dd43b7
zain-ahmed <zain.auk05@gmail.com>
2025-01-09 18:45:48 +0000

    Complete Module 3

---
+++ Foundational C# with Microsoft Course/Course 5/Module 3/Exercise 4/Program.cs
@@ -0,0 +1,31 @@
+﻿string wordInput = "zain";
+string sentenceInput = "there are snakes at the zoo";
+
+Console.WriteLine(wordInput);
+Console.WriteLine(ReverseWord(wordInput));
+Console.WriteLine("");
+Console.WriteLine(sentenceInput);
+Console.WriteLine(ReverseSentence(sentenceInput));
+
+string ReverseWord(string word) 
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--) 
+    {
+        result += word[i];
+    }
+    return result;
+}
+
+string ReverseSentence(string sentence) 
+{
+    string result = "";
+    string[] words = sentence.Split(" ");
+
+    foreach(string word in words) 
+    {
+        result += ReverseWord(word) + " ";
+    }
+
+    return result.Trim();
+}
---
+++ Foundational C# with Microsoft Course/Course 5/Module 3/Exercise 7/Program.cs
@@ -0,0 +1,41 @@
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+
+if (ShouldPlay()) 
+{
+    PlayGame();
+}
+
+bool ShouldPlay()
+{
+    string userInput = Console.ReadLine();
+    return userInput == "Y";
+}
+
+void PlayGame() 
+{
+    var play = true;
+
+    while (play) 
+    {
+        int target;
+        int roll;
+
+        target = random.Next(1, 7);
+        roll = random.Next(1, 7);
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose(target, roll));
+        Console.WriteLine("\nPlay again? (Y/N)");
+        
+        play = ShouldPlay();
+    }
+}
+
+string WinOrLose(int target, int roll)
+{
+    string result = target > roll ? "You win!" : "You lose!";
+    return result;
+}
