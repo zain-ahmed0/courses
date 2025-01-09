82fe9ac78548c24464ea739e5af77fe8f147f362
zain-ahmed <zain.auk05@gmail.com>
2025-01-09 16:28:03 +0000

    Complete Module 2

---
+++ Foundational C# with Microsoft Course/Course 5/Module 2/Exercise 3/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
+
+DisplayStudents(students);
+DisplayStudents(new string[] {"Robert","Vanya"});
+
+void DisplayStudents(string[] students)
+{
+    foreach (string student in students)
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
+*/
+
+double pi = 3.14159;
+PrintCircleArea(12);
+PrintCircleCircumference(24);
+
+void PrintCircleArea(int radius)
+{
+    double area = pi * (radius * radius);
+    Console.WriteLine($"Area = {area}");
+}
+
+void PrintCircleCircumference(int radius)
+{
+    double circumference = 2 * pi * radius;
+    Console.WriteLine($"Circumference = {circumference}");
+}
