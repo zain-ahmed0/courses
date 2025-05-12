c72471e056903aefb8a96a232c31ab7a35456ff1
zain-ahmed <zain.auk05@gmail.com>
2025-05-12 20:59:03 +0100

    Rock Paper Scissors Project - Add DOM

--- The Odin Project/Foundations Course/Course 7/Project - Rock Paper Scissors/script.js
+++ The Odin Project/Foundations Course/Course 7/Project - Rock Paper Scissors/script.js
@@ -1,7 +1,17 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+const result = document.querySelector("body");
+const resultDiv = document.createElement("div");
+
+const score = document.querySelector("body");
+const scoreDiv = document.createElement("div");
+
 let humanScore = 0;
 let computerScore = 0;
 
-const getComputerChoice = () => {
+const getComputerSelection = () => {
 	const result = Math.round(Math.random() * 2);
 	// 0 = rock, 1 = paper, 2 = scissors
 	if (result === 0) {
@@ -13,60 +23,64 @@ const getComputerChoice = () => {
 	}
 };
 
-const getHumanChoice = () => {
-	const answer = prompt("Enter your choice: ");
-	const result = answer.toLowerCase();
-	return result;
-};
-
 const playRound = (humanChoice, computerChoice) => {
-	if (humanChoice === computerChoice) {
-		console.log(`It's a tie! You both chose ${humanChoice}`);
-	} else if (humanChoice === "rock" && computerChoice === "scissors") {
-		console.log(
-			`You win! You chose ${humanChoice}, computer chose ${computerChoice}. Rock crushes scissors.`
-		);
-		humanScore++;
-	} else if (humanChoice === "rock" && computerChoice === "paper") {
-		console.log(
-			`Computer wins! You chose ${humanChoice}, computer chose ${computerChoice}. Paper covers rock.`
-		);
-		computerScore++;
-	} else if (humanChoice === "scissors" && computerChoice === "rock") {
-		console.log(
-			`Computer wins! You chose ${humanChoice}, computer chose ${computerChoice}. Rock crushes scissors.`
-		);
-		computerScore++;
-	} else if (humanChoice === "scissors" && computerChoice === "paper") {
-		console.log(
-			`You win! You chose ${humanChoice}, computer chose ${computerChoice}. Scissors cuts paper.`
-		);
-		humanScore++;
-	} else if (humanChoice === "paper" && computerChoice === "rock") {
-		console.log(
-			`You win! You chose ${humanChoice}, computer chose ${computerChoice}. Paper covers rock.`
-		);
-		humanScore++;
-	} else if (humanChoice === "paper" && computerChoice === "scissors") {
-		console.log(
-			`Computer wins! You chose ${humanChoice}, computer chose ${computerChoice}. Scissors cuts paper.`
-		);
-		computerScore++;
+	if (humanScore != 5 && computerScore != 5) {
+		if (humanChoice === computerChoice) {
+			resultDiv.textContent = `It's a tie! You both chose ${humanChoice}`;
+			result.appendChild(resultDiv);
+		} else if (humanChoice === "rock" && computerChoice === "scissors") {
+			resultDiv.textContent = `You win! You chose ${humanChoice}, computer chose ${computerChoice}. Rock crushes scissors.`;
+			result.appendChild(resultDiv);
+			humanScore++;
+		} else if (humanChoice === "rock" && computerChoice === "paper") {
+			resultDiv.textContent = `Computer wins! You chose ${humanChoice}, computer chose ${computerChoice}. Paper covers rock.`;
+			result.appendChild(resultDiv);
+			computerScore++;
+		} else if (humanChoice === "scissors" && computerChoice === "rock") {
+			resultDiv.textContent = `Computer wins! You chose ${humanChoice}, computer chose ${computerChoice}. Rock crushes scissors.`;
+			result.appendChild(resultDiv);
+			computerScore++;
+		} else if (humanChoice === "scissors" && computerChoice === "paper") {
+			resultDiv.textContent = `You win! You chose ${humanChoice}, computer chose ${computerChoice}. Scissors cuts paper.`;
+			result.appendChild(resultDiv);
+			humanScore++;
+		} else if (humanChoice === "paper" && computerChoice === "rock") {
+			resultDiv.textContent = `You win! You chose ${humanChoice}, computer chose ${computerChoice}. Paper covers rock.`;
+			result.appendChild(resultDiv);
+			humanScore++;
+		} else if (humanChoice === "paper" && computerChoice === "scissors") {
+			resultDiv.textContent = `Computer wins! You chose ${humanChoice}, computer chose ${computerChoice}. Scissors cuts paper.`;
+			result.appendChild(resultDiv);
+			computerScore++;
+		} else {
+			resultDiv.textContent = `Invalid choice: "${humanChoice}". Please choose rock, paper, or scissors.`;
+			result.appendChild(resultDiv);
+		}
+			scoreDiv.textContent = `Current Score - You: ${humanScore}, Computer: ${computerScore}`;
+			score.appendChild(scoreDiv);
 	} else {
-		console.log(
-			`Invalid choice: "${humanChoice}". Please choose rock, paper, or scissors.`
-		);
+		resultDiv.textContent = `Game over!`;
+		result.appendChild(resultDiv);
 	}
-	console.log(
-		`Current Score - You: ${humanScore}, Computer: ${computerScore}`
-	);
 };
 
-const playGame = () => {
-	const humanSelection = getHumanChoice();
-	const computerSelection = "paper";
+rock.addEventListener("click", () => {
+	const humanSelection = "rock";
+	const computerSelection = getComputerSelection();
+
+	playRound(humanSelection, computerSelection);	
+});
+
+paper.addEventListener("click", () => {
+	const humanSelection = "paper";
+	const computerSelection = getComputerSelection();
 
 	playRound(humanSelection, computerSelection);
-};
+});
 
-playGame();
+scissors.addEventListener("click", () => {
+	const humanSelection = "scissors";
+	const computerSelection = getComputerSelection();
+
+	playRound(humanSelection, computerSelection);
+});
