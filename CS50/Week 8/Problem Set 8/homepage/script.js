2153028cca04984848e8e7baeaa6b3d88ad5d92c
zain-ahmed <zain.auk05@gmail.com>
2025-11-12 02:47:58 +0000

    Complete Week 8 - Problem Set 8

---
+++ CS50/Week 8/Problem Set 8/homepage/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+});
